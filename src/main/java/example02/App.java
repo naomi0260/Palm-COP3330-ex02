4b15eb010a5730d3e7330cd44c30fe5ce1e6d247
naomi0260 <84354219+naomi0260@users.noreply.github.com>
2021-06-07 16:33:44 -0400

    Update App.java

--- src/main/java/example02/App.java
+++ src/main/java/example02/App.java
@@ -1,17 +1,8 @@
 package example02;
 
 /*
-Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
-
-Example Output:
-What is the input string? Homer
-Homer has 5 characters.
-
-Constraints:
-Be sure the output contains the original string.
-Use a single output statement to construct the output.
-Use a built-in function of the programming language to determine the length of the string.
-
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Naomi Palm
  */
 
 import java.util.Scanner;
